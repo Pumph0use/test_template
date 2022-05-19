db9795dbc33cf0de81ad2fef4efaf5ad623c1d4a
Dustin Sims <dustinsims0@gmail.com>
2022-05-19 10:49:35 -0500

    new comment

--- alternate.py
+++ alternate.py
@@ -1,2 +1,8 @@
-def test_endpoint(number: int):
+def new_endpoint(number: int):
+    """
+        This is a test doc string
+        :param firstNumber: The first number
+        :param secondNumber: The second number
+        :return: result
+    """
     return number + 1
