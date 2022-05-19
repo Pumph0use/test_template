022abf0ccec2b9f7e8bd47af4f8efdafabe8683a
Dustin Sims <dustinsims0@gmail.com>
2022-05-19 10:49:53 -0500

    new comment

--- alternate.py
+++ alternate.py
@@ -1,8 +1,7 @@
 def new_endpoint(number: int):
     """
         This is a test doc string
-        :param firstNumber: The first number
-        :param secondNumber: The second number
+        :param number: The first number
         :return: result
     """
     return number + 1
