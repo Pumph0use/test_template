b4fc19c2d7c22a1b7f0fdacb8872fede940f79cf
Dustin Sims <dustinsims0@gmail.com>
2022-06-07 14:03:35 -0500

    new comment

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import rasterio
 #aldjal;ksjdasdasdfasdasdfasdfasdasdfasdfadf
 #wee woo wee woo
 def compute(firstNumber: float, secondNumber: float):
-    """
-    This is a test doc string
-    :param firstNumber: The first number
-    :param secondNumber: The second number
-    :return: result
-    """
     # Compute the add
     # Random comment to get a new commit
     # another new commit line
